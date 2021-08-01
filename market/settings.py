9690656cff957578aa0e69802f6604954e4511e2
Jimbo Siona <jsiona@et-n4.kzn.21-school.ru>
2021-08-01 07:23:17 +0300

    add storing shopping carts in sessions

--- market/settings.py
+++ market/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
     'account.apps.AccountConfig',
 ]
 
@@ -127,3 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
