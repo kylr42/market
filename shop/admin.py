04c998cf5f439fc87670c40871a05c64d54b5538
Jimbo Siona <jsiona@et-n4.kzn.21-school.ru>
2021-08-01 06:11:26 +0300

    register catalog models on the admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'get_photo',
+                    'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75px">')
+        return 'Отсутствует'
+    get_photo.short_description = 'Фото'
