c83c3de696432f9bf777bbc8928d97ec80eda309
Jimbo Siona <jsiona@et-n4.kzn.21-school.ru>
2021-08-01 06:04:32 +0300

    add product catalog models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True,
+                            verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True,
+                            verbose_name='Url')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='Url')
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, verbose_name='Фотография')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    available = models.BooleanField(default=True, verbose_name='Наличие')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    category = models.ForeignKey(Category, related_name='products',
+                                 on_delete=models.CASCADE, verbose_name='Категория')
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse('products', kwargs={'slug': self.slug})
