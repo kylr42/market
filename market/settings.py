b86e79c3edf752f5c26f7a79ab22cafa59a97222
Jimbo Siona <jsiona@et-n4.kzn.21-school.ru>
2021-08-01 08:02:14 +0300

    added context processors

--- market/settings.py
+++ market/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
@@ -119,7 +120,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [BASE_DIR / 'market/static', BASE_DIR / 'shop/static']
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
